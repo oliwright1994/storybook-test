e16bd0ee2474bc25637f13aa9edab5a915a62a9f
Luca Gesmundo <lucagesmundo@yahoo.it>
2020-07-10 12:16:52 +0200

    [BRIG-6] export patch (#16)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Table } from './Table'
 export { default as Button } from './Button'
 export { default as Stack } from './Stack'
 export { default as Alert } from './Alert'
+
+export * as PATCH from '@chakra-ui/core'
