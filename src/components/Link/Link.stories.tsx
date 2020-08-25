91d02b0516db151e5e09b899776cb1316abd8eca
Dzianis Spartak <dzianis.spartak@smartpension.co.uk>
2020-08-25 14:18:05 +0300

    [BRIG-15] Create Link Component (#25)
    * [WIP] [BRIG-15] Create Link Component
    
    * [BRIG-15] Tests update
    
    * [BRIG-15] Removed comment
    
    * [BRIG-15] Tests update
    
    * [BRIG-15] Tests update
    
    * [BRIG-15] Linter fix
    
    * [BRIG-15] Removed extra type descriptor
    
    * [BRIG-15] Updated Link story

---
+++ src/components/Link/Link.stories.tsx
@@ -0,0 +1,26 @@
+import { Stack } from '@bright/components'
+import createStory from '@bright/storybook/createStory'
+import { boolean } from '@storybook/addon-knobs'
+import React from 'react'
+
+import Link from '.'
+
+createStory('COMPONENTS | Link', [
+  {
+    label: 'standard',
+    jsx: () => {
+      const internalLink = '/'
+      const externalLink = 'https://www.smart.co/'
+      const isDisabled = boolean('Disabled', false)
+      const isExternal = boolean('External', false)
+
+      return (
+        <Stack>
+          <Link href={isExternal ? externalLink : internalLink} isDisabled={isDisabled} isExternal={isExternal}>
+            {`${isExternal ? 'External' : 'Internal'} ${isDisabled ? 'disabled' : ''} link`}
+          </Link>
+        </Stack>
+      )
+    },
+  },
+])
---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,14 @@
+import { Link as ChakraLink, LinkProps as IChakraLink } from '@chakra-ui/core'
+import React, { FC } from 'react'
+
+interface ILink extends Pick<IChakraLink, 'isExternal' | 'isDisabled' | 'onClick' | 'as'> {
+  href?: string
+}
+
+const Link: FC<ILink> = ({ onClick, href, isExternal, isDisabled, children, as }) => (
+  <ChakraLink onClick={onClick} href={href} isExternal={isExternal} isDisabled={isDisabled} as={as}>
+    {children}
+  </ChakraLink>
+)
+
+export default Link
