2aaff8bbbf140af7b75f8298d8c1a2f491e452dd
Dzianis Spartak <dzianis.spartak@smartpension.co.uk>
2020-10-06 17:58:18 +0300

    [T2-6] Update Card story

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,21 +1,15 @@
 import Tag from '@bright/components/Tag'
 import { MessageType } from '@bright/constants'
-import createStory from '@bright/storybook/createStory'
 import { Text } from '@chakra-ui/core'
+import { Meta } from '@storybook/react/types-6-0'
 import React from 'react'
 
 import Card from './Card'
 
-createStory('COMPONENTS | Card', [
-  {
-    label: 'Card',
-    jsx: () => {
-      return (
-        <Card>
-          <Text>This is Text component inside Card component</Text>
-          <Tag messageType={MessageType.success}>This is Tag component inside Card component</Tag>
-        </Card>
-      )
-    },
-  },
-])
+export default { component: Card, title: 'COMPONENTS / Card' } as Meta
+export const Standard = (): JSX.Element => (
+  <Card>
+    <Text>This is Text component inside Card component</Text>
+    <Tag messageType={MessageType.success}>This is Tag component inside Card component</Tag>
+  </Card>
+)
