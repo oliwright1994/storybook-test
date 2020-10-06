354c6d815a63366d392fd88f08b197e8ca27d403
Dzianis Spartak <dzianis.spartak@smartpension.co.uk>
2020-10-06 17:43:28 +0300

    [T2-6] Update Breadcrumb story

--- src/components/Breadcrumb/Breadcrumb.stories.tsx
+++ src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,21 +1,14 @@
-import createStory from '@bright/storybook/createStory'
+import { Meta } from '@storybook/react/types-6-0'
 import React from 'react'
 
 import Breadcrumb from './Breadcrumb'
 
-createStory('COMPONENTS | Breadcrumb', [
-  {
-    label: 'Breadcrumb',
-    jsx: () => {
-      return (
-        <Breadcrumb>
-          <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-          <Breadcrumb.Item href="/about">About</Breadcrumb.Item>
-          <Breadcrumb.Item href="/contact" isCurrentPage>
-            Contact
-          </Breadcrumb.Item>
-        </Breadcrumb>
-      )
-    },
-  },
-])
+export default { component: Breadcrumb, title: 'COMPONENTS / Breadcrumb' } as Meta
+export const Standard = (): JSX.Element => (
+  <Breadcrumb>
+    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+    <Breadcrumb.Item href="/contact" isCurrentPage>
+      Contact
+    </Breadcrumb.Item>
+  </Breadcrumb>
+)
