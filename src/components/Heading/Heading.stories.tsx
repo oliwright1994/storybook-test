0ffe5b204ab5aeae82a8dda6c8ae941dde8cd56f
Dzianis Spartak <dzianis.spartak@smartpension.co.uk>
2020-10-07 11:50:37 +0300

    [T2-6] Update Heading story

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -1,23 +1,17 @@
-import createStory from '@bright/storybook/createStory'
 import { Stack } from '@chakra-ui/core'
+import { Meta } from '@storybook/react/types-6-0'
 import React from 'react'
 
 import Heading from './Heading'
 
-createStory('COMPONENTS | Heading', [
-  {
-    label: 'Text',
-    jsx: () => {
-      return (
-        <Stack>
-          <Heading as="h1">H1 Heading</Heading>
-          <Heading as="h2">H2 Heading</Heading>
-          <Heading as="h3">H3 Heading</Heading>
-          <Heading as="h4">H4 Heading</Heading>
-          <Heading as="h5">H5 Heading</Heading>
-          <Heading as="h6">H6 Heading</Heading>
-        </Stack>
-      )
-    },
-  },
-])
+export default { component: Heading, title: 'Heading' } as Meta
+export const Standard = (): JSX.Element => (
+  <Stack>
+    <Heading as="h1">H1 Heading</Heading>
+    <Heading as="h2">H2 Heading</Heading>
+    <Heading as="h3">H3 Heading</Heading>
+    <Heading as="h4">H4 Heading</Heading>
+    <Heading as="h5">H5 Heading</Heading>
+    <Heading as="h6">H6 Heading</Heading>
+  </Stack>
+)
