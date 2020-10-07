8daa2f303c12bd242d37d42af897b8db53abf5b6
Dzianis Spartak <dzianis.spartak@smartpension.co.uk>
2020-10-07 12:25:03 +0300

    [T2-6] Update Input story

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,99 +1,24 @@
-import createStory from '@bright/storybook/createStory'
 import { Stack } from '@chakra-ui/core'
-import { boolean } from '@storybook/addon-knobs'
+import { Meta } from '@storybook/react/types-6-0'
 import React from 'react'
 
-import Input from '.'
+import Input, { IInput } from './Input'
 
-createStory('COMPONENTS | Input', [
-  {
-    label: 'standard',
-    jsx: () => {
-      let errorMessage
-      const isDisabled = boolean('Disabled', false)
-      const isInvalid = boolean('Invalid', false)
-      const isRequired = boolean('Required', false)
-      const isFullWidth = boolean('Full width', false)
-      const isReadOnly = boolean('Readonly', false)
-      const placeholders = []
-      if (isDisabled) {
-        placeholders.push('Disabled')
-      }
-      if (isInvalid) {
-        placeholders.push('Invalid')
-        errorMessage = 'This value is invalid.'
-      }
-      if (isRequired) {
-        placeholders.push('Required')
-      }
-      if (isFullWidth) {
-        placeholders.push('Full width')
-      }
-      if (isReadOnly) {
-        placeholders.push('Readonly')
-      }
-      return (
-        <Stack>
-          <Input
-            id="amount"
-            aria-label="label-amount"
-            aria-describedby="label-amount"
-            placeholder={(placeholders.length && placeholders.join(' + ')) || 'basic'}
-            label="Pot value"
-            description="Minimum investment: £5,000.00"
-            isDisabled={isDisabled}
-            isInvalid={isInvalid}
-            isRequired={isRequired}
-            isFullWidth={isFullWidth}
-            isReadOnly={isReadOnly}
-            errorMessage={errorMessage}
-            onChange={() => null}
-          />
-        </Stack>
-      )
-    },
-  },
-  {
-    label: 'personal details',
-    jsx: () => {
-      return (
-        <Stack>
-          <Input
-            id="firstname"
-            aria-label="label-firstname"
-            aria-describedby="label-firstname"
-            placeholder="Firstname"
-            label="Firstname"
-            isRequired
-            isFullWidth={false}
-            value="Jack"
-            onChange={() => null}
-          />
-          <Input
-            id="lastname"
-            aria-label="label-lastname"
-            aria-describedby="label-lastname"
-            placeholder="Lastname"
-            label="Lastname"
-            isRequired
-            isFullWidth={false}
-            value="Smith"
-            onChange={() => null}
-          />
-          <Input
-            id="nin"
-            aria-label="label-nin"
-            aria-describedby="label-nin"
-            placeholder="NIN"
-            label="National Insurance Number"
-            description="For example, AA 12 34 56 B"
-            isDisabled
-            isFullWidth={false}
-            value="SR290397D"
-            onChange={() => null}
-          />
-        </Stack>
-      )
-    },
-  },
-])
+export default { component: Input, title: 'Input' } as Meta
+export const Standard = (args: IInput): JSX.Element => (
+  <Stack>
+    <Input {...args} />
+  </Stack>
+)
+Standard.args = {
+  errorMessage: 'This value is invalid.',
+  isDisabled: false,
+  isInvalid: false,
+  isReadOnly: false,
+  isRequired: false,
+  isFullWidth: false,
+  placeholder: 'basic',
+  label: 'Pot value',
+  id: 'amount',
+  description: 'Minimum investment: £5,000.00',
+}
