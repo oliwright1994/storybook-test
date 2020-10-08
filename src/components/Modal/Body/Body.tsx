3459097779f422e994620966c50ec099e7b30120
oliwright1994 <49248785+oliwright1994@users.noreply.github.com>
2020-10-08 20:02:29 +0100

    go

--- src/components/Modal/Body/Body.tsx
+++
@@ -1,8 +0,0 @@
-import { ModalBody as ChakraModalBody } from '@chakra-ui/core'
-import React, { FC } from 'react'
-
-const Body: FC = ({ children }) => {
-  return <ChakraModalBody>{children}</ChakraModalBody>
-}
-
-export default Body
